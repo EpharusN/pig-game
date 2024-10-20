285c666030060375f2e55f5183ff545c7758a209
Epharus Ndumia <epharusndumia@gmail.com>
2024-10-20 18:35:00 +0300

    handled score for active player

--- starter/script.js
+++ starter/script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 // selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
@@ -15,7 +17,9 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0]; //arrays are 0 based and hence player1 becomes player 0 to respect this logic
 let currentScore = 0;
+let activePlayer = 0;
 
 //rolling dice functionality
 btnRoll.addEventListener('click', function () {
@@ -33,8 +37,15 @@ btnRoll.addEventListener('click', function () {
     //currentScore = currentScore + dice;
     // or
     currentScore += dice;
-    current0El.textContent = currentScore; //change in future to hold dynamically//show the current player
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+    // current0El.textContent = currentScore; //change in future to hold dynamically//show the current player
   } else {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
     //switch to next player
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });
